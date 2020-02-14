8cad4f54803c62a05fa5a2b003240f40012dd40e
Tim Morton <tim.morton@gmail.com>
2020-02-14 13:26:01 -0800

    fixed argument packing

--- lya/data.py
+++ lya/data.py
@@ -3,6 +3,7 @@ from .theory import get_lcdm_pk
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class Data(object):
     def __init__(self, x, y, y_unc):
         self.x = x
@@ -30,6 +31,7 @@ def load_lya_data(filename=lya_data_path):
 
     return data
 
+
 def plot_pk(params=None, filename=lya_data_path):
     """Plot p(k) power spectrum data and theory, with panel for residuals
     
@@ -48,17 +50,16 @@ def plot_pk(params=None, filename=lya_data_path):
 
     if params is None:
         fig, ax = plt.subplots(1, 1)
-        ax.errorbar(data.x, data.y, data.y_unc,
-                color='k', marker="o", ms=5, ls="")
+        ax.errorbar(data.x, data.y, data.y_unc, color="k", marker="o", ms=5, ls="")
 
         return fig
 
     else:
-        ... #TODO: assign this to Aryan
+        ...  # TODO: assign this to Aryan
+
 
 def get_data_transfer_function():
-    
-   
+
     """Returns the data "transfer function" 
 
     Returns
@@ -66,25 +67,17 @@ def get_data_transfer_function():
     data : Data object
         Data object representing the ly-a data divided by LCDM p(k)
     """
-    
-    #Loads experimental lyman alpha P(k)
-    p_data = load_lya_data()
-    
-    #Analytic P(k) from class for cold dark matter w/ corresponing k
-    lcdm_pk = get_lcdm_pk(p_data.x)
-    
-    #Transfer function T(k)
-    tk = np.sqrt(p_data.y / lcdm_pk)
-    
-    #Uncertanty in T(k)
-    tk_unc = .5 * p_data.y_unc / np.sqrt(lcdm_pk * p_data.y)
-    
-    
-    return Data(p_data.x, tk, tk_unc)
 
-  
-    
-    
-    
-    
-    
+    # Loads experimental lyman alpha P(k)
+    p_data = load_lya_data()
+
+    # Analytic P(k) from class for cold dark matter w/ corresponing k
+    lcdm_pk = get_lcdm_pk(p_data.x)
+
+    # Transfer function T(k)
+    tk = np.sqrt(p_data.y / lcdm_pk)
+
+    # Uncertanty in T(k)
+    tk_unc = 0.5 * p_data.y_unc / np.sqrt(lcdm_pk * p_data.y)
+
+    return Data(p_data.x, tk, tk_unc)
