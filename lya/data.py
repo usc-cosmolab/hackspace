df3ecff7822d23f46b7b5ada5330f452b041a4b7
Tim Morton <tim.morton@gmail.com>
2020-04-03 12:15:42 -0700

    fix some things

--- lya/data.py
+++ lya/data.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.rcParams['font.size'] = 13
-plt.rcParams['font.family'] = 'stix'
-plt.rcParams['text.usetex'] = True
-plt.rcParams['figure.figsize'] = (6.5, 5)
-plt.rcParams['figure.dpi'] = 150
-plt.rcParams['lines.color'] = 'k'
+plt.rcParams["font.size"] = 13
+plt.rcParams["font.family"] = "stix"
+plt.rcParams["text.usetex"] = True
+plt.rcParams["figure.figsize"] = (6.5, 5)
+plt.rcParams["figure.dpi"] = 150
+plt.rcParams["lines.color"] = "k"
 
 from .theory import get_theory_pk
-from .conf import lya_data_path
+from .conf import lya_data_path, lcdm_params
 from .theory import get_lcdm_pk
 
 
@@ -41,13 +41,15 @@ def load_lya_data(filename=lya_data_path):
     return data
 
 
-def plot_pk(params=None,
-            filename=lya_data_path,
-            point_color="k",
-            marker_size=5,
-            marker='o',
-            line_color="tab:red",
-            **kwargs):
+def plot_pk(
+    params=None,
+    filename=lya_data_path,
+    point_color="k",
+    marker_size=5,
+    marker="o",
+    line_color="tab:red",
+    **kwargs
+):
     """Plot p(k) power spectrum data and theory, with panel for residuals
 
     Parameters
@@ -69,13 +71,13 @@ def plot_pk(params=None,
 
     # plot the error bar plot in the firs axis
     ax1 = fig.add_subplot(gspec[0:4, :])
-    ax1.errorbar(data.x, data.y, data.y_unc,
-                 color=point_color, ls="", ms=marker_size, marker=marker,
-                 **kwargs)
+    ax1.errorbar(
+        data.x, data.y, data.y_unc, color=point_color, ls="", ms=marker_size, marker=marker, **kwargs
+    )
 
     # add the labels
-    ax1.set_xlabel('$k \\rm [h/Mpc]$')
-    ax1.set_ylabel('$P(k) \\rm[Mpc/h^3]$')
+    ax1.set_xlabel("$k \\rm [h/Mpc]$")
+    ax1.set_ylabel("$P(k) \\rm[Mpc/h^3]$")
 
     if params:
         # add new axis
@@ -84,7 +86,7 @@ def plot_pk(params=None,
         pk = get_theory_pk(data.x, params)
 
         # uncomment this for testing
-        #pk = data.y + 1
+        # pk = data.y + 1
 
         res = data.y - pk
 
@@ -92,13 +94,13 @@ def plot_pk(params=None,
         ax1.plot(data.x, pk, color=line_color, **kwargs)
 
         # plot the residuals in the second axis
-        ax2.errorbar(data.x, res, data.y_unc,
-                     color=point_color, ls="", ms=marker_size, marker=marker,
-                     **kwargs)
+        ax2.errorbar(
+            data.x, res, data.y_unc, color=point_color, ls="", ms=marker_size, marker=marker, **kwargs
+        )
 
         # add labels to the axes
-        ax2.set_xlabel('$k \\rm [h/Mpc$')
-        ax2.set_ylabel('Residual')
+        ax2.set_xlabel("$k \\rm [h/Mpc$")
+        ax2.set_ylabel("Residual")
 
     # remove all whitespace between axes
     fig.subplots_adjust(wspace=0, hspace=0)
@@ -119,7 +121,7 @@ def get_data_transfer_function():
     p_data = load_lya_data()
 
     # Analytic P(k) from class for cold dark matter w/ corresponing k
-    lcdm_pk = get_lcdm_pk(p_data.x)
+    lcdm_pk = get_lcdm_pk(p_data.x / lcdm_params["h"])
 
     # Transfer function T(k)
     tk = np.sqrt(p_data.y / lcdm_pk)
@@ -127,4 +129,4 @@ def get_data_transfer_function():
     # Uncertanty in T(k)
     tk_unc = 0.5 * p_data.y_unc / np.sqrt(lcdm_pk * p_data.y)
 
-    return Data(p_data.x, tk, tk_unc)
+    return Data(p_data.x / lcdm_params["h"], tk, tk_unc)
